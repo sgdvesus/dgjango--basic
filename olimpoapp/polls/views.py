cf337031a10a6a481704cc026bbe7e2a11a6522b
sgdvesus <jesusvlzez@gmail.com>
2022-01-28 20:30:37 -0600

    Creando la vista vote

--- olimpoapp/polls/views.py
+++ olimpoapp/polls/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls.base import reverse
+
+from .models import Choice, Question
 
 def index(resquest):
-    return HttpResponse("Hello world! ")
+    latest_questions_list = Question.objects.all()
+    return render(resquest, "polls/index.html", {
+        "latest_questions_list": latest_questions_list,
+    })
 
-# Create your views here.
+def detail(resquest, question_uid):
+    question = get_object_or_404(Question, pk=question_uid)
+    return render(resquest, "polls/detail.html", {
+        "question": question,
+    })
+
+
+def results(resquest, question_uid):
+    return HttpResponse(f'Results: {question_uid}')
+
+
+def vote(resquest, question_uid):
+    question = get_object_or_404(Question, pk=question_uid)
+    try:
+        selected_choice = question.choice_set.get(pk=resquest.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(resquest, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una opción valida",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,) ))
+
+
+
+
